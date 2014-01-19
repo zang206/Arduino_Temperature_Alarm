e7981a6c67b04be52ed6faf156e1a1c5e64d2a9f
zang206 <jprocknow@gmail.com>
2014-01-19 01:21:12 -0600

    Update temp_alarm_ver_2_0.ino

--- temp_alarm_ver_2_0.ino
+++ temp_alarm_ver_2_0.ino
@@ -70,22 +70,27 @@ long MaxTempF = 75
 
 void setup(void)
 {
-  // initialize the digital pin as an output.
+  // initialize the digital pins as inputs or outputs.
   pinMode(buzzer_pin, OUTPUT);
   pinMode(dualLED_green_pin, OUTPUT);
   pinMode(dualLED_red_pin, OUTPUT);
   pinMode(snooze_pin, INPUT);
   
-  digitalWrite(buzzer_pin, LOW);
-  digitalWrite(dualLED_green_pin, LOW);   // turn the LED off
-  digitalWrite(dualLED_red_pin, LOW);   // turn the LED off
+  //define the inital states of the digital outputs
+  digitalWrite(buzzer_pin, LOW); //turn buzzer off. set high to turn on buzzer.
+  //the dualLED is a 2 pin LED with two LEDs connected in parallel in opposite polarities
+  //so only one can be on at one time. radio shack part# 276-012 ie if you want green then
+  //green_pin must be high and red_pin must be low.  vise versa for red. if both are low or
+  //high the LED will be off.
+  digitalWrite(dualLED_green_pin, LOW);   // turn the LED off if both low
+  digitalWrite(dualLED_red_pin, LOW);   // turn the LED off if both low
   
   Serial.begin(9600);  //start the serial port for the debug terminal
   Serial.println("MintDuino Temperature Alarm");
 
   7segDisplay.begin(0x70);  //Start the 4 character, 7-segment display
   
-  TempSensor.begin();  // Start up the Dallas library
+  TempSensor.begin();  // Start up the Dallas devices library for the DS18B20
 }
 
 
@@ -94,7 +99,7 @@ void loop(void)
   // call TempSensor.requestTemperatures() to issue a global temperature 
   // request to all devices on the bus
   Serial.print("Requesting temperatures...");
-  TempSensor.requestTemperatures(); // Send the command to get temperatures
+  TempSensor.requestTemperatures(); // Send the command to get temperature from DS18B20
   Serial.println("DONE");
   Serial.print("Temperature for the device 1 (index 0) is: ");
   Serial.println(sensors.getTempFByIndex(0));
@@ -107,18 +112,7 @@ void loop(void)
     {
       Serial.println("button pressed");
       
-    7segDisplay.blinkRate(1);
-    /*digitalWrite(buzzer_pin, HIGH);   // turn the LED on (HIGH is the voltage level)
-    digitalWrite(dualLED_green_pin, HIGH);   // turn the LED on (HIGH is the voltage level)
-    digitalWrite(dualLED_red_pin, LOW);   // turn the LED on (HIGH is the voltage level)
-    delay(500);*/               // wait for a second
-    digitalWrite(buzzer_pin, LOW);    // turn the LED off by making the voltage LOW
-    digitalWrite(dualLED_green_pin, LOW);    // turn the LED off by making the voltage LOW
-    digitalWrite(dualLED_red_pin, HIGH);    // turn the LED off by making the voltage LOW
-    delay(300);               // wait for a second
-    digitalWrite(buzzer_pin, HIGH);   // turn the LED on (HIGH is the voltage level)
-    digitalWrite(dualLED_green_pin, HIGH);   // turn the LED on (HIGH is the voltage level)
-    digitalWrite(dualLED_red_pin, LOW);   // turn the LED on (HIGH is the voltage level)
+    
   }
   else
   {
@@ -132,4 +126,23 @@ void loop(void)
   //delay(500);
   
   }
+}
+
+function readSnoozeButton(int button_pin)
+{
+  return digitalRead(button_pin);
+}
+
+function soundSiren()
+{
+  7segDisplay.blinkRate(1);
+  digitalWrite(buzzer_pin, HIGH);   // turn the buzzer on
+  digitalWrite(dualLED_green_pin, HIGH);   // turn the green LED on
+  digitalWrite(dualLED_red_pin, LOW);   // turn the red LED off
+  delay(500);*/               // wait for half a second
+  digitalWrite(buzzer_pin, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(dualLED_green_pin, LOW);    // turn the green LED off by making the voltage LOW
+  digitalWrite(dualLED_red_pin, HIGH);    // turn the red LED on by making the voltage HIGH
+  //delay(300);               // wait for a third of a second
+
 }
